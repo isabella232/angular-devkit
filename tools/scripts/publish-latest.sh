0195008de9f50e9d5ded2a5b9235f2410ff409dd
Suguru Inatomi <suguru.inatomi@gmail.com>
2020-12-01 18:12:04 +0900

    ci(publish): run yarn script at root context

--- tools/scripts/publish-latest.sh
+++ tools/scripts/publish-latest.sh
@@ -5,7 +5,6 @@ project=$1
 echo "Publishing ${project}"
 
 yarn build ${project} --prod --with-deps
-cd dist/projects/${project}
-yarn can-npm-publish && yarn publish --access public
+yarn can-npm-publish dist/projects/${project} && yarn --cwd dist/projects/${project} publish --access public
 
 echo "Done"
